34e33debb7d0775ae59326251e76224378840309
Lev Vereshchagin <mail@vrslev.com>
2022-02-27 17:53:58 +0300

    Add !! and !$

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,6 +30,54 @@ set -x PATH $HOME/.local/bin $PATH
 function fish_greeting
 end
 
+function bind_bang
+    switch (commandline --current-token)[-1]
+    case "!"
+        # Without the `--`, the functionality can break when completing
+        # flags used in the history (since, in certain edge cases
+        # `commandline` will assume that *it* should try to interpret
+        # the flag)
+        commandline --current-token -- $history[1]
+        commandline --function repaint
+    case "*"
+        commandline --insert !
+    end
+end
+
+# Add !! and !$
+# https://github.com/fish-shell/fish-shell/issues/288#issuecomment-591679913
+function bind_bang
+    switch (commandline --current-token)[-1]
+    case "!"
+        commandline --current-token -- $history[1]
+        commandline --function repaint
+    case "*"
+        commandline --insert !
+    end
+end
+
+function bind_dollar
+    switch (commandline --current-token)[-1]
+    case "*!\\"
+        commandline --current-token -- (echo -ns (commandline --current-token)[-1] | head -c '-1')
+        commandline --insert '$'
+    case "!"
+        commandline --current-token ""
+        commandline --function history-token-search-backward
+
+    case "*!"
+        commandline --current-token -- (echo -ns (commandline --current-token)[-1] | head -c '-1')
+        commandline --function history-token-search-backward
+    case "*"
+        commandline --insert '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
+
 alias pip=pip3
 alias ls=exa
 alias pc=pre-commit
