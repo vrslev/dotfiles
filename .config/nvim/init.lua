19cbe35875027555d9cc5bda6cd8fd358ac5b8ee
Lev Vereshchagin <mail@vrslev.com>
2022-08-07 18:52:29 +0300

    Clean

--- .config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require("vrslev.theme")
-require("vrslev.options")
-require("vrslev.packer")
---require("vrslev.mason")
--- require("vrslev.cmp")
---local result = require("mason-lspconfig").get_installed_servers()
---for i,v in ipairs(result) do print(i,v) end
