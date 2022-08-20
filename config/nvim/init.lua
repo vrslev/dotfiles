5053f856d6967830135e88821117f06824c5c392
Lev Vereshchagin <mail@vrslev.com>
2022-08-20 19:01:54 +0300

    u

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,12 +1,1 @@
-pcall(require, "impatient")
-local bootstrapping = require("vrslev.packer")
-
-if not bootstrapping then
-    require("vrslev.options")
-    require("vrslev.theme")
-    require("vrslev.statusline")
-    require("vrslev.cmp")
-    require("vrslev.lsp")
-    require("vrslev.telescope")
-    require("vrslev.treesitter")
-end
+require("vrslev")
