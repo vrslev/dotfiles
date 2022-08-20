cde14d0273c2a1637308824f4476d911895fe5e2
Lev Vereshchagin <mail@vrslev.com>
2022-08-20 20:04:18 +0300

    u

--- config/nvim/lua/vrslev/init.lua
+++ config/nvim/lua/vrslev/init.lua
@@ -1,13 +1,12 @@
 pcall(require, "impatient")
+
+require("vrslev.options")
+
 local bootstrapping = require("vrslev.packer")
 
 if not bootstrapping then
-    require("vrslev.options")
     require("vrslev.theme")
-    require("vrslev.statusline")
     require("vrslev.cmp")
     require("vrslev.lsp")
-    require("vrslev.telescope")
     require("vrslev.treesitter")
-    require("vrslev.filetree")
 end
--- config/nvim/lua/vrslev/packer.lua
+++ config/nvim/lua/vrslev/packer.lua
@@ -1,34 +1,36 @@
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-
 packer_bootstrap = false
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  packer_bootstrap = vim.fn.system(
-    {'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path}
-    )
+  packer_bootstrap = vim.fn.system {
+    'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path
+}
   vim.cmd [[packadd packer.nvim]]
   vim.cmd [[autocmd User PackerComplete quitall]]
 end
 
-require("packer").init {
-    autoremove = true,
-}
-require("packer").startup(function(use)
+local packer = require("packer")
+packer.init { autoremove = true }
+packer.startup(function(use)
     use "wbthomason/packer.nvim"
 
     use 'lewis6991/impatient.nvim'
     use "projekt0n/github-nvim-theme"
+    use {"nvim-lualine/lualine.nvim", config = function() require("vrslev.plugins.lualine") end}
     use "gpanders/editorconfig.nvim"
     use 'nvim-lua/plenary.nvim'
-    use 'nvim-telescope/telescope.nvim'
-    use "nvim-lualine/lualine.nvim"
-    
+    use {'nvim-telescope/telescope.nvim', config = function() require("vrslev.plugins.telescope") end}
     use {
-        {"nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate"
-        },
+        "nvim-neo-tree/neo-tree.nvim",
+        requires = {"MunifTanjim/nui.nvim"},
+        config = function() require("vrslev.plugins.neo-tree") end,
+    }
+    use {"windwp/nvim-autopairs", config = function() require("vrslev.plugins.nvim-autopairs") end}
+
+    use {
+        {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"},
         "nvim-treesitter/nvim-treesitter-textobjects",
-        "nvim-treesitter/nvim-treesitter-context"
+        "nvim-treesitter/nvim-treesitter-context",
     }
 
     use {
@@ -41,7 +43,7 @@ require("packer").startup(function(use)
         "ray-x/lsp_signature.nvim",
         "j-hui/fidget.nvim",
         { "folke/trouble.nvim", "kyazdani42/nvim-web-devicons" },
-        {"RishabhRD/lspactions", 'nvim-lua/popup.nvim'}
+        {"RishabhRD/lspactions", 'nvim-lua/popup.nvim'},
     }
     
     use {
@@ -51,24 +53,15 @@ require("packer").startup(function(use)
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-nvim-lua',
         "hrsh7th/cmp-cmdline",
-
+        
         'L3MON4D3/LuaSnip',
         'rafamadriz/friendly-snippets',
-
+        
         "onsails/lspkind.nvim"
     }
 
-    use {
-        "nvim-neo-tree/neo-tree.nvim",
-        "MunifTanjim/nui.nvim",
-    }
-
-    use {
-        "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup() end
-    }
     if packer_bootstrap then
-        require("packer").sync()
+        packer.sync()
     end
 end)
 
