9f060dc3b22242d387fc16bd540c4a38fa6c2471
Lev Vereshchagin <mail@vrslev.com>
2022-08-26 17:25:06 +0300

    Update auto dark mode in Wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,25 +1,12 @@
 local wezterm = require 'wezterm'
 
-function scheme_for_appearance(appearance)
-    if appearance:find 'Dark' then
-        return 'GitHub Dark'
-    else
-        return 'GitHub Light'
-    end
+local function scheme_for_appearance(appearance)
+    return appearance:find 'Dark' and "GitHub Dark" or "GitHub Light"
 end
-
-wezterm.on('window-config-reloaded', function(window, pane)
-    local overrides = window:get_config_overrides() or {}
-    local appearance = window:get_appearance()
-    local scheme = scheme_for_appearance(appearance)
-    if overrides.color_scheme ~= scheme then
-        overrides.color_scheme = scheme
-        window:set_config_overrides(overrides)
-    end
-end)
 
 return {
     check_for_updates = false,
+    color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
     font = wezterm.font 'JetBrains Mono',
     font_size = 14,
     hyperlink_rules = {
