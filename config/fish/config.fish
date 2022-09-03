cabfd5dfdd79ff41630f1cf67ae0fcf25c99d0f3
Lev Vereshchagin <mail@vrslev.com>
2022-09-03 20:29:24 +0300

    Use z

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -12,7 +12,9 @@ else
 end
 
 pyenv init - | source
-eval (starship init fish)
+starship init fish | source
+zoxide init fish | source
+wezterm shell-completion --shell fish | source
 
 # Add pipx binaries
 set -x PATH $HOME/.local/bin $PATH
@@ -33,19 +35,6 @@ set -x DARK_MODE (defaults read -globalDomain AppleInterfaceStyle 2>/dev/null | 
 function fish_greeting
 end
 
-function bind_bang
-    switch (commandline --current-token)[-1]
-    case "!"
-        # Without the `--`, the functionality can break when completing
-        # flags used in the history (since, in certain edge cases
-        # `commandline` will assume that *it* should try to interpret
-        # the flag)
-        commandline --current-token -- $history[1]
-        commandline --function repaint
-    case "*"
-        commandline --insert !
-    end
-end
 
 # Add !! and !$
 # https://github.com/fish-shell/fish-shell/issues/288#issuecomment-591679913
