037ebdbca56aa5935bc8049a7aac060fa4c837ee
Lev <mail@vrslev.com>
2023-12-26 12:56:11 +0300

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -289,16 +289,17 @@ require("lazy").setup({
                     desc = 'LSP actions',
                     callback = function(event)
                         local opts = { buffer = event.buf }
+                        local telescope = require('telescope.builtin')
 
                         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                        vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-                        vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+                        vim.keymap.set("n", "gd", telescope.lsp_definitions, opts)
+                        vim.keymap.set("n", "gi", telescope.lsp_implementations, opts)
+                        vim.keymap.set("n", "gr", telescope.lsp_references, opts)
+                        vim.keymap.set("n", "<leader>ld", telescope.diagnostics, opts)
                         vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
                         vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-                        vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-                        vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-                        vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+                        vim.keymap.set("n", "<leader>lf", vim.lsp.buf.code_action, opts)
+                        vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
                         vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
                     end
                 })
