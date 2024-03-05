fb3e29b3f2c866ab0dcf0c57e7f62c19fb846ace
Lev Vereshchagin <mail@vrslev.com>
2024-03-05 21:27:34 +0300

    Remove nvim config

--- config/nvim/README.md
+++
@@ -1,6 +0,0 @@
-# My Neovim config
-
-Inspired by:
-- https://github.com/ThePrimeagen/init.lua
-- https://github.com/LazyVim/LazyVim
-- https://github.com/nvim-lua/kickstart.nvim
--- config/nvim/lua/vrslev/autocmds.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-	callback = function()
-		if not vim.bo.readonly and vim.fn.expand("%") ~= "" then
-			vim.cmd("silent update")
-		end
-	end,
-})
