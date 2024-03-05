95c494bbb4844308694c18755ebcbac8cd13cd87
Lev Vereshchagin <mail@vrslev.com>
2024-03-05 21:26:49 +0300

    Improve fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,64 +1,54 @@
-# Init brew env
+# $PATH and essentials
 if test -f /opt/homebrew/bin/brew
-    # arm64
-    eval (/opt/homebrew/bin/brew shellenv)
-    set -x CPPFLAGS "-I/opt/homebrew/include -L/opt/homebrew/lib"
+    /opt/homebrew/bin/brew shellenv | source
+    set -gx CPPFLAGS -I/opt/homebrew/include -L/opt/homebrew/lib
 end
 
-# Preferred editor for local and remote sessions
-if test -n $SSH_CONNECTION
-    set -x EDITOR code
-else
-    set -x EDITOR nano
-end
+set pythons ~/Library/Application\ Support/hatch/pythons/*/python/bin
 
-set -x PATH ~/.local/bin $PATH  # Add pipx binaries
-# Add scipts
-set -x PATH ~/code/dotfiles/bin $PATH
-# Add cargo
-set -x PATH ~/.cargo/bin $PATH
-# Add Rancher
-set -x PATH ~/.rd/bin $PATH
-
-# Add python interpreters provided by hatch
-for dir in ~/Library/Application\ Support/hatch/pythons/*/python/bin
-    set -x PATH $dir $PATH
-end
+fish_add_path ~/code/dotfiles/bin\
+    ~/.local/bin\
+    ~/.cargo/bin\
+    ~/.rd/bin\
+    pythons[-1..1]
 
 zoxide init fish | source
 starship init fish | source
-wezterm shell-completion --shell fish | source
 
-# Prevent python from writing byte code
-set -x PYTHONDONTWRITEBYTECODE 1
-# Set language
-set -x LANG en_US.UTF-8
-set -x LANGUAGE $LANG
-set -x LC_ALL $LANG
-# Set non-legacy mode for fish plugin jethrokuan/fzf
-set -U FZF_LEGACY_KEYBINDINGS 0
+if test -f ~/.fish_profile
+  source ~/.fish_profile
+end
 
-set -x GOPATH ~/.go
+function fish_greeting # No greeting when starting an interactive shell
+end
 
-set -x HOMEBREW_BUNDLE_NO_LOCK 1
-
-set -x DARK_MODE (defaults read -globalDomain AppleInterfaceStyle 2>/dev/null | grep Dark)
-
-if [ "$DARK_MODE" = "Dark" ]
-    set -x BAT_THEME "Visual Studio Dark+"
-    set -x DELTA_FEATURES "dark"
+# Environment variables
+if test -n $SSH_CONNECTION
+    set -gx EDITOR code
 else
-    set -x BAT_THEME GitHub
-    set -x DELTA_FEATURES "light"
+    set -gx EDITOR nano
 end
 
-# No greeting when starting an interactive shell
-function fish_greeting
+set -gx PYTHONDONTWRITEBYTECODE 1
+set -gx LANG en_US.UTF-8
+set -gx LANGUAGE $LANG
+set -gx LC_ALL $LANG
+set -gx GOPATH ~/.go
+set -gx HOMEBREW_BUNDLE_NO_LOCK 1
+
+set -gx DARK_MODE (defaults read -globalDomain AppleInterfaceStyle 2>/dev/null)
+
+if test "$DARK_MODE" = Dark
+    set -gx BAT_THEME Visual Studio Dark+
+    set -gx DELTA_FEATURES dark
+else
+    set -gx BAT_THEME GitHub
+    set -gx DELTA_FEATURES light
 end
 
+# Aliases and abbreviations
 
-# Add !! and !$
-# https://github.com/fish-shell/fish-shell/issues/288#issuecomment-591679913
+# Add !$ (https://github.com/fish-shell/fish-shell/issues/288#issuecomment-591679913)
 function bind_bang
     switch (commandline --current-token)[-2]
     case "!"
@@ -86,10 +76,8 @@ function bind_dollar
     end
 end
 
-function fish_user_key_bindings
-    bind ! bind_bang
-    bind '$' bind_dollar
-end
+bind ! bind_bang
+bind '$' bind_dollar
 
 alias ls "exa --icons"
 alias l ls
@@ -102,12 +90,9 @@ alias venv "uv venv"
 alias python python3
 alias py python3
 alias gr 'cd $(git rev-parse --show-toplevel)'
+
 abbr g git
 abbr pc pre-commit
 abbr dco "docker compose"
 abbr po poetry
 abbr j just
-
-if test -f ~/.fish_profile
-  source ~/.fish_profile
-end
