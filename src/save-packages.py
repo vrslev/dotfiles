3bd060a990ec533ec35300bed23daf7c4dc25eca
Lev Vereshchagin <mail@vrslev.com>
2024-03-09 13:02:44 +0300

    Refactor save-packages

--- src/save-packages.py
+++ src/save-packages.py
@@ -1,30 +1,15 @@
 import subprocess
 from pathlib import Path
-from typing import Iterable
 
+lines = subprocess.check_output(("pipx", "list", "--short")).decode().splitlines()
+packages = [line.split(" ")[0] for line in lines]
 
-def get_pipx_packages() -> Iterable[str]:
-    out = subprocess.check_output(("pipx", "list", "--short"))
-    for line in out.decode().splitlines():
-        yield line.split(" ")[0]
+path = Path(__file__).parent.parent / "config" / "packages" / "pipx.txt"
+content = "\n".join(packages) + "\n"
 
+with path.open("w+") as f:
+    f.seek(0)
+    f.write(content)
+    f.truncate()
 
-def overwrite_file(path: Path, content: str) -> None:
-    with path.open("w+") as f:
-        f.seek(0)
-        f.write(content + "\n")
-        f.truncate()
-
-
-def save_packages_list(package_manager_name: str, packages: list[str]) -> None:
-    print(f"Saved {package_manager_name} packages: {packages}")
-    path = (
-        Path(__file__).parent.parent
-        / "config"
-        / "packages"
-        / f"{package_manager_name}.txt"
-    )
-    overwrite_file(path, "\n".join(packages))
-
-
-save_packages_list("pipx", list(get_pipx_packages()))
+print(f"Saved pipx packages: {packages}")
