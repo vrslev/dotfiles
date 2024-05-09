2674420d5e2d47b783045892894d2700bb182511
Lev Vereshchagin <mail@vrslev.com>
2024-05-09 15:43:45 +0300

    Remove hyperlink_rules

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -8,36 +8,6 @@ config.check_for_updates = false
 config.color_scheme = "Vs Code Dark+ (Gogh)"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 15
-config.hyperlink_rules = {
-	-- Linkify things that look like URLs and the host has a TLD name.
-	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-	{
-		regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
-		format = "$0",
-	},
-
-	-- linkify email addresses
-	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-	{
-		regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
-		format = "mailto:$0",
-	},
-
-	-- file:// URI
-	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-	{
-		regex = [[\bfile://\S*\b]],
-		format = "$0",
-	},
-
-	-- Linkify things that look like URLs with numeric addresses as hosts.
-	-- E.g. http://127.0.0.1:8000 for a local development server,
-	-- or http://192.168.1.1 for the web interface of many routers.
-	{
-		regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
-		format = "$0",
-	},
-}
 config.initial_cols = 130
 config.initial_rows = 36
 config.window_decorations = "RESIZE"
