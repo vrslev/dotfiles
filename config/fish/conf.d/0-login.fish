457ac1837cf2214759a38ec392ce4d285be60023
Lev Vereshchagin <mail@vrslev.com>
2024-10-26 18:16:42 +0300

    Use mise for env vars

--- config/fish/conf.d/0-login.fish
+++ config/fish/conf.d/0-login.fish
@@ -2,31 +2,16 @@ if not status is-login
   exit
 end
 
-if not type -q /opt/homebrew/bin/brew
-  echo No brew installed!
-  exit 1
+if type -q /opt/homebrew/bin/brew
+  /opt/homebrew/bin/brew shellenv | source
+else
+  echo "brew not found"
 end
 
-set -gx LANG en_US.UTF-8
-set -gx LANGUAGE $LANG
-set -gx LC_ALL $LANG
-set -gx EDITOR code
-set -gx PYTHONDONTWRITEBYTECODE 1
-
-/opt/homebrew/bin/brew shellenv | source
-set -gx HOMEBREW_BUNDLE_NO_LOCK 1
-set -gx HOMEBREW_NO_ANALYTICS 1
-set -gx HOMEBREW_NO_AUTO_UPDATE 1
-set -gx HOMEBREW_NO_ENV_HINTS 1
-set -gx HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK 1
-set -gx CPPFLAGS -I/opt/homebrew/include
-set -gx LDFLAGS -L/opt/homebrew/lib
-set -gx UV_NATIVE_TLS true
-set -gx MISE_ENVIRONMENT dev  # tell mise to use ~/.config/mise/config.dev.toml
-
-# allow vscode to detect tools
-if not set --query DIRENV_INITIALIZED
+if type -q ~/.local/bin/mise
+  if not set --query DIRENV_INITIALIZED
   ~/.local/bin/mise hook-env --shell fish | source
+  end
+else
+  echo "mise not found"
 end
-
-set -gx DOTFILES_ROOT (dirname (dirname (dirname (readlink (dirname (status --current-filename))))))
