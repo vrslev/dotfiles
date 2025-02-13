b58374c3007d02429db786b4c721a0bb831e73f6
Lev Vereshchagin <mail@vrslev.com>
2025-02-13 11:34:10 +0300

    Add run-in-dirs script

--- config/fish/conf.d/0-login.fish
+++ config/fish/conf.d/0-login.fish
@@ -12,6 +12,8 @@ end
 if type -q mise
   if not set --query DIRENV_INITIALIZED
     mise hook-env --shell fish | source
+    # Fix mise when running parallel
+    set -x MISE_CACHE_DIR ~/Library/Caches/mise
   end
 else
   echo "mise not found"
