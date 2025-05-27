5ce552613b868fc0d5fffc24bf450215886ee626
Lev Vereshchagin <mail@vrslev.com>
2025-05-27 21:15:29 +0300

    Optimize mise shell startup

--- home/.config/fish/conf.d/0-login.fish
+++ home/.config/fish/conf.d/0-login.fish
@@ -2,6 +2,9 @@ if not status is-login
   exit
 end
 
+set -x MISE_CACHE_DIR ~/Library/Caches/mise  # Fix mise when running parallel
+set -x MISE_FISH_AUTO_ACTIVATE "0" # Disable auto-activation script coming from brew
+
 if type -q /opt/homebrew/bin/brew
   /opt/homebrew/bin/brew shellenv | source
 else
@@ -9,12 +12,12 @@ else
 end
 
 # make sure VS Code can detect tools
-if type -q mise
-  if not set --query DIRENV_INITIALIZED
-    mise hook-env --shell fish | source
-    # Fix mise when running parallel
-    set -x MISE_CACHE_DIR ~/Library/Caches/mise
+if not status is-interactive
+  if type -q mise
+    if not set --query DIRENV_INITIALIZED
+      mise hook-env --shell fish | source
+    end
+  else
+    echo "mise not found"
   end
-else
-  echo "mise not found"
 end
