29bafd190ddeb510867462551e458f0347375d34
Lev Vereshchagin <mail@vrslev.com>
2025-06-02 19:16:33 +0300

    Fix slow shell startup

--- home/.config/fish/conf.d/0-login.fish
+++ home/.config/fish/conf.d/0-login.fish
@@ -2,11 +2,17 @@ if not status is-login
   exit
 end
 
-set -x MISE_CACHE_DIR ~/Library/Caches/mise  # Fix mise when running parallel
+set -x MISE_CACHE_DIR ~/Library/Caches/mise  # Fix mise when running `parallel`
 set -x MISE_FISH_AUTO_ACTIVATE "0" # Disable auto-activation script coming from brew
 
 if type -q /opt/homebrew/bin/brew
-  /opt/homebrew/bin/brew shellenv | source
+  # /opt/homebrew/bin/brew shellenv | pbcopy
+  set --global --export HOMEBREW_PREFIX "/opt/homebrew";
+  set --global --export HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+  set --global --export HOMEBREW_REPOSITORY "/opt/homebrew";
+  fish_add_path --global --move --path "/opt/homebrew/bin" "/opt/homebrew/sbin";
+  if test -n "$MANPATH[1]"; set --global --export MANPATH '' $MANPATH; end;
+  if not contains "/opt/homebrew/share/info" $INFOPATH; set --global --export INFOPATH "/opt/homebrew/share/info" $INFOPATH; end;
 else
   echo "brew not found"
 end
