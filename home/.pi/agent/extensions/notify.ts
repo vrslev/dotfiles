39fdcbeb6f7963937acc250d82b8714d439de762
Lev Vereshchagin <mail@vrslev.com>
2026-02-27 13:18:05 +0300

    Update

--- home/.pi/agent/extensions/notify.ts
+++ home/.pi/agent/extensions/notify.ts
@@ -1,6 +1,15 @@
 export default function (pi) {
+  const IGNORED_ERROR_MESSAGES = [
+    "429 status code (no body)",
+  ];
+
   // @ts-ignore
-  pi.on("agent_end", async (_event, ctx) => {
-    await pi.exec("afplay", ["/System/Library/Sounds/Morse.aiff"]);
+  pi.on("agent_end", async (event) => {
+    const lastMsg = event.messages[event.messages.length - 1];
+    const errorMsg = lastMsg?.role === "assistant" ? lastMsg.errorMessage : null;
+    // @ts-ignore
+    if (!IGNORED_ERROR_MESSAGES.includes(errorMsg)) {
+      await pi.exec("afplay", ["/System/Library/Sounds/Morse.aiff"]);
+    }
   });
 }
