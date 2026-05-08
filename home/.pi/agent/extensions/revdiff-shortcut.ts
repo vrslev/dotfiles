732c262875b992c4381275362ef96a54a56b3b62
Lev Vereshchagin <mail@vrslev.com>
2026-05-08 14:19:47 +0300

    Restore editor text after running /revdiff

--- home/.pi/agent/extensions/revdiff-shortcut.ts
+++ home/.pi/agent/extensions/revdiff-shortcut.ts
@@ -2,8 +2,12 @@ export default function (pi) {
   pi.registerShortcut("super+r", {
     description: "Run /revdiff",
     handler: async (ctx) => {
+      const saved = ctx.ui.getEditorText();
       ctx.ui.setEditorText("/revdiff");
-      setImmediate(() => process.stdin.emit("data", "\r"));
+      setImmediate(() => {
+        process.stdin.emit("data", "\r");
+        setImmediate(() => ctx.ui.setEditorText(saved));
+      });
     },
   });
 }
