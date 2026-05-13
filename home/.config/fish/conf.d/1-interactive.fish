21c69e6f30477dd8bfffc8a78d79b1fd1c89f6d4
Lev Vereshchagin <mail@vrslev.com>
2026-05-13 21:34:16 +0300

    Update

--- home/.config/fish/conf.d/1-interactive.fish
+++ home/.config/fish/conf.d/1-interactive.fish
@@ -17,22 +17,7 @@ if type -q fzf
   fzf --fish | source
 end
 
-abbr --add b brew
-abbr --add m mise
-abbr --add dco "docker compose"
-abbr --add g git
-abbr --add j just
-abbr --add l ls
-abbr --add po poetry
-abbr --add py python3
-abbr --add rmt trash
-abbr --add c z
-
 alias cd z
-alias dir-size 'du -sh'
-alias git-root 'cd $(git rev-parse --show-toplevel)'
-alias ls "eza --icons --all"
-alias ll "ls --long"
 alias posix 'exec bash -c "$argv; exec fish"'
 
 function beep
